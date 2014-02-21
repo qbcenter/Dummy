a752290cd268c0b19053a20684f86e097d9755b6
Peter Chen <peter.chen@skysource.com.tw>
2014-02-21 11:00:06 +0800

    Update to UTF-8 (with Chinese words)

--- SP020_ModifyStaffGroupItem.sql
+++ SP020_ModifyStaffGroupItem.sql
@@ -1,9 +1,19 @@
-/****** Object:  StoredProcedure [bonus].[SP020_ModifyStaffGroupItem]    Script Date: 03/14/2012 20:58:29 ******/
-SET ANSI_NULLS ON
-GO
+Ôªø/****** Object:  StoredProcedure bonus.SP020_ModifyStaffGroupItem    Script Date: 03/14/2012 20:58:29 ******/
+if exists 
+(
+  select * 
+  from sys.objects 
+  where object_id = object_id('bonus.SP020_ModifyStaffGroupItem') 
+  and type in ('P', 'PC')
+)
+drop procedure bonus.SP020_ModifyStaffGroupItem
+go
 
-SET QUOTED_IDENTIFIER ON
-GO
+set ansi_nulls on
+go
+
+set quoted_identifier on
+go
 
 -- ===================================================================
 -- NAME:
@@ -13,198 +23,225 @@ GO
 --   Skysource Technologies (c) 2011
 -- 
 -- DESCRIPTION:
---   Update or insert staff group item
+--   update or insert staff group item
 -- 
 -- HISTORY:
 --   2011-07-19 Livia Wu
 --   File created.
---   2011-12-12 Tony Chui
---   Modify on Update method
+--   2011-12-12 Tony Chi
+--   Modify on update method
 --   2012-03-09   Martin Ku
 --   Remove usage on STAFF_GROUP_MAP for group item. We use STAFF_MAP instead.
 -- ===================================================================
 
-CREATE PROCEDURE [bonus].[SP020_ModifyStaffGroupItem]
+create procedure bonus.SP020_ModifyStaffGroupItem
   @STAFF_GROUP_ITEM_KEY int = null,
   @STAFF_GROUP_ITEM_NAME nvarchar(50) = null,
   @STAFF_GROUP_ID nvarchar(50) = null,
   @MONTH_START_DATE datetime = null,
   @UPDATE_BY_ID nvarchar(50) = null,
   @strActionType nvarchar(50) = null
-AS
-  DECLARE @intstaffGroupItemKey nvarchar(50) = null
-  DECLARE @intstaffGroupItemHistoryKey int = null
-  DECLARE @dtEffectiveStartDate datetime = null
-  DECLARE @intstaffGroupMapHistoryKey int = null
-BEGIN
-  -- SET NOCOUNT ON added to prevent extra result sets from interfering with SELECT statements.
-  SET NOCOUNT ON;
+as
+  declare @intstaffGroupItemHistoryKey int = null
+  declare @dtEffectiveStartDate datetime = null
+  declare @intStaffMapKey int = null
+  declare @strStaffGroupID nvarchar(50) = null
+  declare @strBrandID nvarchar(50) = null
+begin
+  -- set NOCOUNT ON added to prevent extra result sets from interfering with select statements.
+  set NOCOUNT on;
 
-  -- get current histroy record's history key and effective start date
-  SELECT @intstaffGroupItemHistoryKey = MAX(STAFF_GROUP_ITEM_HISTORY_KEY), @dtEffectiveStartDate = EFFECTIVE_START_DATE
-  FROM bonus.STAFF_GROUP_ITEM_HISTORY
-  WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-  AND EFFECTIVE_START_DATE <= @MONTH_START_DATE
-  AND EFFECTIVE_END_DATE > @MONTH_START_DATE
-  GROUP BY EFFECTIVE_START_DATE
+  -- get current history record's history key and effective start date
+  select 
+    @intstaffGroupItemHistoryKey = max(STAFF_GROUP_ITEM_HISTORY_KEY), 
+    @strStaffGroupID = max(STAFF_GROUP_ID),
+    @dtEffectiveStartDate = EFFECTIVE_START_DATE
+  from bonus.STAFF_GROUP_ITEM_HISTORY
+  where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+  and EFFECTIVE_START_DATE <= @MONTH_START_DATE
+  and EFFECTIVE_END_DATE > @MONTH_START_DATE
+  group by EFFECTIVE_START_DATE
 
-  IF @strActionType = 'UPS'
-  BEGIN
-    IF @STAFF_GROUP_ITEM_KEY IS NULL
-    BEGIN
-      SELECT @intstaffGroupItemKey = STAFF_GROUP_ITEM_KEY 
-      FROM bonus.STAFF_GROUP_ITEM 
-      WHERE STAFF_GROUP_ITEM_NAME = @STAFF_GROUP_ITEM_NAME
-    END
-    ELSE 
-    BEGIN
-      SELECT @intstaffGroupItemKey = @STAFF_GROUP_ITEM_KEY 
-    END
+  if @strActionType = 'UPS'
+  begin
     
-    -- Insert new Staff Class    
-    IF @intstaffGroupItemKey IS NOT NULL AND @STAFF_GROUP_ITEM_NAME IS NOT NULL AND @intstaffGroupItemHistoryKey IS NOT NULL
-    BEGIN
-    -- UPDATE    
-
-      UPDATE bonus.STAFF_GROUP_ITEM
-      SET 
+    -- Insert new Staff Group Item  
+    if @STAFF_GROUP_ITEM_KEY is not null and @STAFF_GROUP_ITEM_NAME is not null and @intstaffGroupItemHistoryKey is not null
+    begin
+    -- update
+      update bonus.STAFF_GROUP_ITEM
+      set 
         STAFF_GROUP_ITEM_NAME = @STAFF_GROUP_ITEM_NAME, 
         ACTIVE_FLAG = 'True',
-        UPDATE_TIMESTAMP = GETDATE(),
+        UPDATE_TIMESTAMP = getdate(),
         UPDATE_BY_ID = @UPDATE_BY_ID
-      WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+      where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
       
-
-    --¨Oß_¨∞∑Ì§Î
-    --¨O
-      IF @dtEffectiveStartDate = @MONTH_START_DATE
-      BEGIN
-        UPDATE bonus.STAFF_GROUP_ITEM_HISTORY
-        SET 
+    --ÊòØÂê¶ÁÇ∫Áï∂Êúà
+    --ÊòØ
+      if @dtEffectiveStartDate = @MONTH_START_DATE
+      begin
+        update bonus.STAFF_GROUP_ITEM_HISTORY
+        set 
           STAFF_GROUP_ITEM_NAME = @STAFF_GROUP_ITEM_NAME, 
-          UPDATE_TIMESTAMP = GETDATE(),
+          UPDATE_TIMESTAMP = getdate(),
           UPDATE_BY_ID = @UPDATE_BY_ID
-        WHERE STAFF_GROUP_ITEM_HISTORY_KEY = @intstaffGroupItemHistoryKey
-      END
-      ELSE IF @dtEffectiveStartDate <> @MONTH_START_DATE
-      BEGIN
-        UPDATE bonus.STAFF_GROUP_ITEM_HISTORY
-        SET 
+        where STAFF_GROUP_ITEM_HISTORY_KEY = @intstaffGroupItemHistoryKey
+      end
+    --Âê¶
+      else
+      begin
+        update bonus.STAFF_GROUP_ITEM_HISTORY
+        
+        set 
           EFFECTIVE_END_DATE = @MONTH_START_DATE, 
-          UPDATE_TIMESTAMP = GETDATE(),
+          UPDATE_TIMESTAMP = getdate(),
           UPDATE_BY_ID = @UPDATE_BY_ID
-        WHERE STAFF_GROUP_ITEM_HISTORY_KEY = @intstaffGroupItemHistoryKey
+        where STAFF_GROUP_ITEM_HISTORY_KEY = @intstaffGroupItemHistoryKey
         
-        INSERT INTO bonus.STAFF_GROUP_ITEM_HISTORY
+        insert into bonus.STAFF_GROUP_ITEM_HISTORY
           (STAFF_GROUP_ITEM_KEY, STAFF_GROUP_ITEM_NAME, STAFF_GROUP_ID, EFFECTIVE_START_DATE, EFFECTIVE_END_DATE, CREATE_TIMESTAMP, CREATE_BY_ID)
-        VALUES (@intstaffGroupItemKey, @STAFF_GROUP_ITEM_NAME, @STAFF_GROUP_ID, @MONTH_START_DATE, '9999-12-31', GETDATE(), @UPDATE_BY_ID)      
-      END
-    END
-    ELSE IF @STAFF_GROUP_ITEM_NAME IS NOT NULL AND @intstaffGroupItemHistoryKey IS NULL
-    BEGIN
+        values (@STAFF_GROUP_ITEM_KEY, @STAFF_GROUP_ITEM_NAME, @strStaffGroupID, @MONTH_START_DATE, '9999-12-31', getdate(), @UPDATE_BY_ID)
+      end
+    end
+    else if @STAFF_GROUP_ITEM_NAME is not null and @STAFF_GROUP_ID is not null and @intstaffGroupItemHistoryKey is null
+    begin
+      update bonus.STAFF_GROUP_ITEM
+      set 
+        STAFF_GROUP_ITEM_NAME = @STAFF_GROUP_ITEM_NAME, 
+        STAFF_GROUP_ID = @STAFF_GROUP_ID,
+        ACTIVE_FLAG = 'True',
+        UPDATE_TIMESTAMP = getdate(),
+        UPDATE_BY_ID = @UPDATE_BY_ID
+      where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+      if @@ROWCOUNT = 0
+      -- Insert a new one if not existed
+      begin
+        insert into bonus.STAFF_GROUP_ITEM
+          (STAFF_GROUP_ITEM_NAME, STAFF_GROUP_ID, ACTIVE_FLAG, CREATE_TIMESTAMP, CREATE_BY_ID)
+        values (@STAFF_GROUP_ITEM_NAME, @STAFF_GROUP_ID, 'True', getdate(), @UPDATE_BY_ID)    
+        insert into bonus.STAFF_GROUP_ITEM_HISTORY
+          (STAFF_GROUP_ITEM_KEY, STAFF_GROUP_ITEM_NAME, STAFF_GROUP_ID, EFFECTIVE_START_DATE, EFFECTIVE_END_DATE, CREATE_TIMESTAMP, CREATE_BY_ID)
+        values (SCOPE_IDENTITY(), @STAFF_GROUP_ITEM_NAME, @STAFF_GROUP_ID, @MONTH_START_DATE, '9999-12-31', getdate(), @UPDATE_BY_ID) 		
+      end
+      else
+      begin
+        insert into bonus.STAFF_GROUP_ITEM_HISTORY
+          (STAFF_GROUP_ITEM_KEY, STAFF_GROUP_ITEM_NAME, STAFF_GROUP_ID, EFFECTIVE_START_DATE, EFFECTIVE_END_DATE, CREATE_TIMESTAMP, CREATE_BY_ID)
+        values (@STAFF_GROUP_ITEM_KEY, @STAFF_GROUP_ITEM_NAME, @STAFF_GROUP_ID, @MONTH_START_DATE, '9999-12-31', getdate(), @UPDATE_BY_ID)        
+      end
+    end
+  end   
+  else if @strActionType = 'DEL'
+  begin
+  --Âà™Èô§Â∞èÁµÑË≤†Ë≤¨‰∫∫Âì° 
+    --ÈÅ∏ÂèñÂ∞èÁµÑË≤†Ë≤¨‰∫∫Âì°
+    declare staffMapKeycursor cursor for
+    select STAFF_MAP_KEY from bonus.STAFF_MAP
+    where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+    and STAFF_MAP_TYPE_ID = 'SM002'
+    and ACTIVE_FLAG = 'True'
 
-      INSERT INTO bonus.STAFF_GROUP_ITEM
-        (STAFF_GROUP_ITEM_NAME, STAFF_GROUP_ID, ACTIVE_FLAG, CREATE_TIMESTAMP, CREATE_BY_ID)
-      VALUES (@STAFF_GROUP_ITEM_NAME, @STAFF_GROUP_ID, 'True', GETDATE(), @UPDATE_BY_ID)       
-        
-      INSERT INTO bonus.STAFF_GROUP_ITEM_HISTORY
-        (STAFF_GROUP_ITEM_KEY, STAFF_GROUP_ITEM_NAME, STAFF_GROUP_ID, EFFECTIVE_START_DATE, EFFECTIVE_END_DATE, CREATE_TIMESTAMP, CREATE_BY_ID)
-      VALUES (SCOPE_IDENTITY(), @STAFF_GROUP_ITEM_NAME, @STAFF_GROUP_ID, @MONTH_START_DATE, '9999-12-31', GETDATE(), @UPDATE_BY_ID)        
-    END
-  END    
-  ELSE IF @strActionType = 'DEL'
-  BEGIN
-    IF @dtEffectiveStartDate = @MONTH_START_DATE
-    BEGIN    
-      --ßR∞£§p≤’≠t≥d§H≠˚
-      BEGIN
-        DELETE bonus.STAFF_MAP_HISTORY
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-        AND STAFF_MAP_TYPE_ID = 'SM002' --¿Á∑~¬æ≠˚∫[©±¬d™¯≠t≥d§p≤’
-        AND EFFECTIVE_START_DATE = @MONTH_START_DATE
-
-        DELETE bonus.STAFF_MAP
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-        AND STAFF_MAP_TYPE_ID = 'SM002'
-        AND ACTIVE_FLAG = 'True'
-      END      
+    --ÈñãÂïüË≥áÊñôÂ∫´cursor
+    open staffMapKeycursor
+    fetch next from staffMapKeycursor into @intStaffMapKey
+    while @@FETCH_STATUS = 0
+    begin
+      exec bonus.SP010_ModifyStaffMap
+      @STAFF_MAP_KEY = @intStaffMapKey,
+      @MONTH_START_DATE = @MONTH_START_DATE,
+      @UPDATE_BY_ID = @UPDATE_BY_ID,
+      @strActionType = 'DEL'
+      fetch next from staffMapKeycursor into @intStaffMapKey
+    end
+    close staffMapKeycursor
+    deallocate staffMapKeycursor
     
-      DELETE FROM bonus.STAFF_GROUP_ITEM_HISTORY
-      WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-      AND EFFECTIVE_START_DATE = @MONTH_START_DATE              
+  --Âà™Èô§Â∞èÁµÑË≤†Ë≤¨ÂìÅÁâå
+    --ÈÅ∏ÂèñÂ∞èÁµÑÊâÄÂ±¨ÂìÅÁâå
+    declare staffGroupMapKeycursor cursor for
+    select STAFF_GROUP_ID, BRAND_ID from bonus.STAFF_GROUP_MAP
+    where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+    and STAFF_GROUP_MAP_TYPE_ID = 'SGM002'
+    and ACTIVE_FLAG = 'True'
+
+    --ÈñãÂïüË≥áÊñôÂ∫´cursor
+    open staffGroupMapKeycursor
+    fetch next from staffGroupMapKeycursor into @strStaffGroupID, @strBrandID
+    while @@FETCH_STATUS = 0
+    begin
+      exec bonus.SP005_ModifyStaffGroupMap
+      @STAFF_GROUP_ID = @strStaffGroupID,
+      @BRAND_ID = @strBrandID,
+      @STAFF_GROUP_MAP_TYPE_ID = 'SGM002',
+      @STAFF_GROUP_ITEM_KEY = null,
+      @MONTH_START_DATE = @MONTH_START_DATE,
+      @UPDATE_BY_ID = @UPDATE_BY_ID,
+      @strActionType = 'UPS'
+      fetch next from staffGroupMapKeycursor into @strStaffGroupID, @strBrandID
+    end
+    close staffGroupMapKeycursor
+    deallocate staffGroupMapKeycursor
+    
+    if @dtEffectiveStartDate = @MONTH_START_DATE
+    begin    
+      delete from bonus.STAFF_GROUP_ITEM_HISTORY
+      where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+      and EFFECTIVE_START_DATE = @MONTH_START_DATE              
     
       -- Type 2, get remaining inactivated historical data
 
-      SELECT @intstaffGroupItemHistoryKey = MAX(STAFF_GROUP_ITEM_HISTORY_KEY)
-      FROM bonus.STAFF_GROUP_ITEM_HISTORY
-      WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+      select @intstaffGroupItemHistoryKey = max(STAFF_GROUP_ITEM_HISTORY_KEY)
+      from bonus.STAFF_GROUP_ITEM_HISTORY
+      where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
       
       -- Type 3, no inactivated historical data
       -- Type 2, historical data exist, reset current record
-      IF @intstaffGroupItemHistoryKey IS NULL
-      BEGIN
-        DELETE FROM bonus.STAFF_GROUP_ITEM
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY        
-      END      
-      ELSE
-      BEGIN
-        UPDATE bonus.STAFF_GROUP_ITEM
-        SET
-          STAFF_GROUP_ITEM_NAME = 
-          (
-            SELECT STAFF_GROUP_ITEM_NAME 
-            FROM bonus.STAFF_GROUP_ITEM_HISTORY
-            WHERE STAFF_GROUP_ITEM_HISTORY_KEY = @intstaffGroupItemHistoryKey
-          ),
+      if @intstaffGroupItemHistoryKey is null
+      begin
+        delete from bonus.STAFF_GROUP_ITEM
+        where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY        
+      end      
+      else
+      begin
+        update bonus.STAFF_GROUP_ITEM
+        set
+          STAFF_GROUP_ITEM_NAME = t1.STAFF_GROUP_ITEM_NAME,
+          STAFF_GROUP_ID = t1.STAFF_GROUP_ID,
           ACTIVE_FLAG = 'False',
-          UPDATE_TIMESTAMP = GETDATE(),
+          UPDATE_TIMESTAMP = getdate(),
           UPDATE_BY_ID = @UPDATE_BY_ID
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-      END      
-    END
+        from
+        (
+          select
+            STAFF_GROUP_ITEM_NAME,
+            STAFF_GROUP_ID
+          from bonus.STAFF_GROUP_ITEM_HISTORY
+          where STAFF_GROUP_ITEM_HISTORY_KEY = @intstaffGroupItemHistoryKey
+        ) t1
+        where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+      end      
+    end
     -- Type 1, update exiting records in current and history table for logical delete
-    ELSE
-    BEGIN
-      --ßR∞£§p≤’≠t≥d§H≠˚
-      BEGIN
-        UPDATE bonus.STAFF_MAP
-        SET
-        ACTIVE_FLAG = 'False'
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-        AND STAFF_MAP_TYPE_ID = 'SM002'
-
-        UPDATE bonus.STAFF_MAP_HISTORY
-        SET
-        EFFECTIVE_END_DATE = @MONTH_START_DATE,
-        UPDATE_TIMESTAMP = GETDATE(),
+    else
+    begin
+      update bonus.STAFF_GROUP_ITEM
+      set
+        ACTIVE_FLAG = 'False',
+        UPDATE_TIMESTAMP = getdate(),
         UPDATE_BY_ID = @UPDATE_BY_ID
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-        AND STAFF_MAP_TYPE_ID = 'SM002'
-        AND EFFECTIVE_START_DATE <= @MONTH_START_DATE
-        AND EFFECTIVE_END_DATE > @MONTH_START_DATE
-      END     
+      where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
       
-      BEGIN
-        UPDATE bonus.STAFF_GROUP_ITEM
-        SET
-          ACTIVE_FLAG = 'False',
-          UPDATE_TIMESTAMP = GETDATE(),
-          UPDATE_BY_ID = @UPDATE_BY_ID
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-      
-        UPDATE bonus.STAFF_GROUP_ITEM_HISTORY
-        SET
-          EFFECTIVE_END_DATE = @MONTH_START_DATE,
-          UPDATE_TIMESTAMP = GETDATE(),
-          UPDATE_BY_ID = @UPDATE_BY_ID
-        WHERE STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
-        AND EFFECTIVE_START_DATE <= @MONTH_START_DATE
-        AND EFFECTIVE_END_DATE > @MONTH_START_DATE
-      END
-    END
-  END
-END
+      update bonus.STAFF_GROUP_ITEM_HISTORY
+      set
+        EFFECTIVE_END_DATE = @MONTH_START_DATE,
+        UPDATE_TIMESTAMP = getdate(),
+        UPDATE_BY_ID = @UPDATE_BY_ID
+      where STAFF_GROUP_ITEM_KEY = @STAFF_GROUP_ITEM_KEY
+      and EFFECTIVE_START_DATE <= @MONTH_START_DATE
+      and EFFECTIVE_END_DATE > @MONTH_START_DATE
+    end
+  end
+end
 
-GO
-
-
+go
